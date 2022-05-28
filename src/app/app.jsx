a723aa2720636c568d96dbd64b946bbb0fa01e78
leny <info@flatland.be>
2022-05-29 00:17:09 +0200

    Base dumb code for app

---
+++ src/app/app.jsx
@@ -0,0 +1,12 @@
+/* leny/aklèt
+ *
+ * /src/app/app.jsx - App Entrypoint
+ *
+ * coded by leny
+ * started at 29/05/2022
+ */
+
+import {createRoot} from "react-dom";
+
+const root = createRoot(document.querySelector("#app"));
+root.render(<p>{"Hello, Aklèt!"}</p>);
