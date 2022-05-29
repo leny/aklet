abadc366b07ff79d0e8b643e386b6b9a3fde6ca5
leny <info@flatland.be>
2022-05-29 14:16:06 +0200

    Add constants

--- src/core/constants.js
+++ src/core/constants.js
@@ -9,3 +9,10 @@
 export const DEBUG = process.env.NODE_ENV !== "production";
 
 export const SERVER_ROOT = DEBUG ? "http://localhost:3000" : "/";
+
+export const MODE_MENU = "mode.menu";
+export const MODE_GAME = "mode.game";
+export const MODE_RESULTS = "mode.results";
+
+export const BSP = "\u0020";
+export const NBSP = "\u00a0";
