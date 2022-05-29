b6602011b5db749de2a73dd12f9e26e75865523e
leny <info@flatland.be>
2022-05-29 18:42:32 +0200

    Fix tooling

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,4 +8,20 @@
 
 module.exports = {
     extends: "@leny",
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
+        babelOptions: {
+            presets: ["@babel/preset-react"],
+        },
+    },
+    rules: {
+        "no-console": 0,
+        "react/jsx-max-depth": 0,
+        "react/no-array-index-key": 0,
+    },
 };
